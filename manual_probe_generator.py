74fde213cd308525294839c2c8a022c13c09e723
Kieran Aponte <rilakumafan@gmail.com>
2023-09-02 16:09:58 -0700

    Added description & attribution

--- manual_probe_generator.py
+++ manual_probe_generator.py
@@ -1,25 +1,49 @@
+"""
+Copyright (c) 2023 Kieran Aponte
+This software is licensed under the GPL V3 License.
+"""
+
+# The probe files generated from g-code-ripper often probe outside of the stock 
+# due to lead-ins in the g code, so manual_probe_generator.py accepts the raw 
+# stock dimensions and leaves a buffer for safety. While probing, record the 
+# z values in stock_probe_W_xx_L_yy_data.txt. You can now use this instead of 
+# steps 1-3 listed in README.MD.
+
+# To ensure that your tool or collet does not collide with your clamps, please 
+# use gcode_boundaries.py with your desired G-Code files: 
+# https://github.com/Miimiikiu/gcode-boundaries
+
 ####USER VARIABLES####
 
 # Assumes mm
-stock_width = 77     # stock size along x
-stock_length = 130
+
+stock_width = 76     # stock size along x
+stock_length = 304   # stock size along y
 probe_points_x = 2   # number of points to probe along x
-probe_points_y = 3   
+probe_points_y = 4   # number of points to probe along y
 z_safe = 15          # height of travel moves
 z_probe_start = 5    # height immediately before probe
 feed_rate_fast = 500 # feed rate during travel
 feed_rate_slow = 20  # feed rate during probe
+clamp_buffer_x = 0   # buffer should be >= 0
+clamp_buffer_y = 20  # buffer should be >= 0
 
 
 wcs_x = 0 # 0 = stock center, -stock_width/2 = stock left, stock-width/2 = stock right
 wcs_y = -stock_length/2 # 0 = stock center, -stock_length/2 = stock bottom, stock-length/2 = stock top
 
-path = 'G:\\My Drive\\NC Files\\Lathe Adapter\\Clamps\\stock_probe_W_{}_L_{}.nc'.format(stock_width, stock_length)
+path = 'G:\\My Drive\\NC Files\\Lathe Adapter V2\\6.5mm Stock\\stock_probe_W_{}_L_{}.nc'.format(stock_width, stock_length)
 
 ######################
 
 buffer_x = (stock_width / 2) / probe_points_x # Buffer prevents G31 outside of stock from lead-in moves in g-code ripper or dangerously close to stock edge
 buffer_y = (stock_length / 2) / probe_points_y
+
+# raise generated buffers if collision with clamps may otherwise occur
+if buffer_x < clamp_buffer_x:
+    buffer_x = clamp_buffer_x 
+if buffer_y < clamp_buffer_y:
+    buffer_y = clamp_buffer_y
 
 with open(path[:-3] + '_data.txt', 'w') as datafile: #create blank file for manual data entry
     
